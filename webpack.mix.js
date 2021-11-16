135af23674ff5caf053c80e8f5fd436fd5246206
Bernhard Hörmann <hoermann.b@gmail.com>
2021-11-16 10:18:51 +0100

    INIT: Install jetstream and basic config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
+mix.js('resources/js/app.js', 'public/mix/js')
+    .postCss('resources/css/app.css', 'public/mix/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
