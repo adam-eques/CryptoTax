a58a1f273d781652fe8f3f173fa21afbb94dd435
Bernhard Hörmann <hoermann.b@gmail.com>
2021-11-23 12:56:00 +0100

    CHG: Refactored card and wallet index

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,15 @@
+<div {{ $attributes->merge(["class" => "bg-white border border-gray-300 rounded overflow-hidden"]) }}>
+    {{-- Heading --}}
+    <div class="rounded-t bg-color-2 flex justify-between py-2 lg:py-4 px-4 lg:px-6 items-center border-b border-gray-300">
+        <div class="sm:space-y-1">
+            <h3 class="sm:text-xl uppercase text-gray-700 font-bold">{{ $title }}</h3>
+            @if(isset($subtitle)) <p class="text-gray-400 text-md">{{ $subtitle }}</p> @endif
+        </div>
+        @if(isset($right)) <div class="sm:text-xl text-gray-700 font-semibold">{{ $right }}</div> @endif
+    </div>
+
+    {{-- Body --}}
+    <div class="flex flex-col divide-y divide-gray-300 scrollbar overflow-auto max-h-100">
+        {{ $slot }}
+    </div>
+</div>
--- resources/views/pages/wallets/index.blade.php
+++ resources/views/pages/wallets/index.blade.php
@@ -21,7 +21,7 @@
                                             <h3 class="xl:text-xl font-semibold text-gray-700">{{ $item["name"] }}</h3>
                                             <p class="text-gray-400 text-xs xl:text-md">Updating...</p>
                                         </div>
-                                        <p class="xl:text-xl text-gray-700  font-semibold">${{ number_format($item["price"], 2) }}</p>
+                                        <p class="xl:text-xl text-gray-700 font-semibold">${{ number_format($item["price"], 2) }}</p>
                                     </div>
                                 @endforeach
                             </div>
@@ -32,32 +32,21 @@
 
             <!-- Right Panel -->
             <div class="px-2 lg:px-5 md:w-3/5">
-
-                <div class="bg-white border border-gray-300 rounded overflow-hidden">
-                    <!-- Heading -->
-                    <div class="rounded-t bg-color-2 flex justify-between py-2 lg:py-4 px-4 lg:px-6 items-center border-b border-gray-300">
-                        <div class="sm:space-y-1">
-                            <h3 class="sm:text-xl uppercase text-gray-700  font-bold">Coinbase</h3>
-                            <p class="text-gray-400 text-md">Updating...</p>
-                        </div>
-                        <p class="sm:text-xl text-gray-700  font-semibold">$698,189.000</p>
-                    </div>
-                    <div class="flex flex-col divide-y divide-gray-300 scrollbar overflow-auto max-h-100">
-                        @php
-                            $coins = [
-                                ["transactions" => 102, "price" => 20213, "amount" => 0.019848, "name" => "BTC"],
-                                ["transactions" => 45, "price" => 12456, "amount" => 2.025, "name" => "LTC"],
-                                ["transactions" => 88, "price" => 1.03, "amount" => 22.025, "name" => "XRP"],
-                                ["transactions" => 54, "price" => 2.85743, "amount" => 100.025, "name" => "MIOTA"],
-                                ["transactions" => 57, "price" => 25.045, "amount" => 5.77, "name" => "DASH"],
-                                ["transactions" => 258, "price" => 2258.6878, "amount" => 6.7889, "name" => "ETH"],
-                            ];
-                        @endphp
-                        @foreach($coins as $coin)
-                            <x-coin-row :coin="$coin"></x-coin-row>
-                        @endforeach
-                    </div>
-                </div>
+                <x-card title="Coinbase" subtitle="Updating..." right="$698,189.000">
+                    @php
+                        $coins = [
+                            ["transactions" => 102, "price" => 20213, "amount" => 0.019848, "name" => "BTC"],
+                            ["transactions" => 45, "price" => 12456, "amount" => 2.025, "name" => "LTC"],
+                            ["transactions" => 88, "price" => 1.03, "amount" => 22.025, "name" => "XRP"],
+                            ["transactions" => 54, "price" => 2.85743, "amount" => 100.025, "name" => "MIOTA"],
+                            ["transactions" => 57, "price" => 25.045, "amount" => 5.77, "name" => "DASH"],
+                            ["transactions" => 258, "price" => 2258.6878, "amount" => 6.7889, "name" => "ETH"],
+                        ];
+                    @endphp
+                    @foreach($coins as $coin)
+                        <x-coin-row :coin="$coin"></x-coin-row>
+                    @endforeach
+                </x-card>
             </div>
         </div>
     </x-page>
