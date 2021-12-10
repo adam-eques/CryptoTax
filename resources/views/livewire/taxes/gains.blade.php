624fbcd60ae835e7118e517574d2e3e60858a02e
Koala-Dev-dream <koalah86@gmail.com>
2021-12-10 08:05:19 -0800

    CREATE:Portfolio:Income, Gains

---
+++ resources/views/livewire/taxes/gains.blade.php
@@ -0,0 +1,32 @@
+<div class="p-3 border rounded-md">
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <h5 class="py-5 text-xl font-bold">Capital Gains</h5>
+    <div>
+        <table class="w-full py-4">
+            <thead class="text-left bg-gray-100"> 
+                <tr>
+                    <th class="py-5 px-2">Value</th>
+                    <th class="py-5 px-2">Short Term</th>
+                    <th class="py-5 px-2">Long Term</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="border-b">
+                    <td class="py-5 px-2">Crypto to crypto gains</td>
+                    <td class="py-5 px-2">$ 5020.00</td>
+                    <td class="py-5 px-2">$ 90250.00</td>
+                </tr>
+                <tr class="border-b">
+                    <td class="py-5 px-2">Other capital gains</td>
+                    <td class="py-5 px-2">$ 1520.00</td>
+                    <td class="py-5 px-2">$ 90250.00</td>
+                </tr>
+                <tr class="font-bold text-xl">
+                    <td class="py-5 px-2">Total capital gains</td>
+                    <td class="py-5 px-2">$ 1520.00</td>
+                    <td class="py-5 px-2">$ 90250.00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
