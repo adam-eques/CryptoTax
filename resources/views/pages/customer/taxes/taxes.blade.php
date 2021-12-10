7a9512d8cfa4c786c6a842632d809f499a3f442a
Crazy_Boy <75461450+Koala-Dev-dream@users.noreply.github.com>
2021-12-10 08:16:52 -0800

    Revert "CREATE:Portfolio:Income, Gains"

--- resources/views/pages/customer/taxes/taxes.blade.php
+++ resources/views/pages/customer/taxes/taxes.blade.php
@@ -1,12 +1,5 @@
 <x-app-layout>
     <div class="mx-auto my-5 px-3 xs:px-4 xl:max-w-screen-2xl lg:px-5 py-5 bg-white rounded-sm shadow">
        @livewire('taxes.header')
-       <div class="grid grid-cols-1 md:grid-cols-12 gap-0 md:gap-4">
-           <div class="col-span-7 space-y-5">
-               @livewire('taxes.gains')
-               @livewire('taxes.income')
-           </div>
-           <div class="col-span-5"></div>
-       </div>
     </div>
 </x-app-layout>
