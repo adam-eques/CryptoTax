6e5ddfca0ee15914da5e33d36ff61f57b3f376c6
Koala-Dev-dream <koalah86@gmail.com>
2021-12-09 17:41:44 -0800

    Fix:Portfolio:Linechart

--- resources/views/livewire/portfolio/linechart.blade.php
+++ resources/views/livewire/portfolio/linechart.blade.php
@@ -18,9 +18,76 @@
         </div>
     </div>
     <div class="px-4 h-90">
-        <livewire:livewire-line-chart
-            key="{{ $lineChartModel->reactiveKey() }}"
-            :line-chart-model="$lineChartModel"
-        />
+        <div id="chart"></div>
     </div>
 </div>
+@push('scripts')
+<script>
+    (function () {
+        var options = {
+            chart: {
+                height: "100%",
+                type: "area",
+                stacked: false,
+                chartTopMargin: "40",
+                toolbar:{
+                    show: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            colors: ["#796cff", "#a69dff"],
+            series: [
+                {
+                    data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+                },
+                {
+                    data: [2.6, 3.3, 3.8, 2.7, 3.6, 3.9, 5.2, 5.9]
+                }
+            ],
+            stroke: {
+                curve: 'smooth',
+                dashArray: [0, 8],
+                width: 2
+            },
+            plotOptions: {
+                
+            },
+            fill: {
+                type: "gradient",
+                colors: ['#796cff', '#eae8fe'],
+                opacity:[0.8],
+                gradient: {
+                    shadeIntensity: 1,
+                    opacityFrom: 0.7,
+                    opacityTo: 0.9,
+                    stops: [0, 90, 100]
+                }
+            },
+            xaxis: {
+                categories: ["Sep 2019", "Oct 2019", "Nov 2019", "Dec 2019", "Jan 2020", "Feb 2020", "Mar 2020", "Apr 2020"]
+            },
+            yaxis: {
+                axisTicks: {
+                    show: true
+                },
+                axisBorder: {
+                    show: true,
+                    color: "#cfcfcf"
+                },
+                labels: {
+                        style: {
+                        colors: "#cfcfcf"
+                    }
+                },
+            },
+            legend: {
+                show: false
+            }
+        };
+        const chart = new ApexCharts(document.getElementById(`chart`), options);
+        chart.render();
+    }());
+</script>
+@endpush
