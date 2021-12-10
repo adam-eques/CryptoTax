66e0876cc374940a2edc95c3e2c4dc20e48e168d
Koala-Dev-dream <koalah86@gmail.com>
2021-12-10 04:01:01 -0800

    DELETE:livewire-chart

--- app/Http/Livewire/Portfolio/Situation.php
+++ app/Http/Livewire/Portfolio/Situation.php
@@ -2,48 +2,12 @@
 
 namespace App\Http\Livewire\Portfolio;
 
-use App\Models\Expense;
-use Asantibanez\LivewireCharts\Facades\LivewireCharts;
-
 use Livewire\Component;
 
 class Situation extends Component
 {
-    public $types = ['Sep11', 'Sep12', 'Sep13', 'Sep14', 'Sep15'];    
-
-    public $firstRun = true;
-
-    public $showDataLabels = false;
     public function render()
     {
-        $expenses = Expense::whereIn('type', $this->types)->get();
-
-        $lineChartModel = $expenses
-            ->reduce(function ($lineChartModel, $data) use ($expenses) {
-                $index = $expenses->search($data);
-
-                $amountSum = $expenses->take($index + 1)->sum('amount');
-
-                if ($index == 6) {
-                    $lineChartModel->addMarker(7, $amountSum);
-                }
-
-                if ($index == 11) {
-                    $lineChartModel->addMarker(12, $amountSum);
-                }
-
-                return $lineChartModel->addPoint($index, $data->amount, ['id' => $data->id]);
-            }, LivewireCharts::lineChartModel()
-                ->setAnimated($this->firstRun)
-                ->setSmoothCurve()
-                ->setXAxisVisible(true)
-                ->setDataLabelsEnabled($this->showDataLabels)
-                ->sparklined()
-            );
-        $this->firstRun = false;
-        return view('livewire.portfolio.situation')
-        ->with([
-            'lineChartModel' => $lineChartModel,
-        ]);;
+        return view('livewire.portfolio.situation');
     }
 }
