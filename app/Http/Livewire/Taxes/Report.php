5b6308d2acfc69f15f43af54c84812b3b4a68d76
Koala-Dev-dream <koalah86@gmail.com>
2021-12-10 16:37:39 -0800

    CREATE:Tax Page

---
+++ app/Http/Livewire/Taxes/Report.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Taxes;
+
+use Livewire\Component;
+
+class Report extends Component
+{
+    public function render()
+    {
+        return view('livewire.taxes.report');
+    }
+}
