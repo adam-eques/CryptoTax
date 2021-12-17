996e190cc19da039af8f6dce08945837e8a0486e
Koala-Dev-dream <koalah86@gmail.com>
2021-12-17 05:48:14 -0800

    FIX:Dashboard:about

--- resources/views/livewire/dashboard/about.blade.php
+++ resources/views/livewire/dashboard/about.blade.php
@@ -1,4 +1,4 @@
-<div class=" bg-bgcolor1 rounded-md text-white w-full xl:px-20 md:px-12 xl:py-22 md:py-10 px-5 py-5 relative">
+<div class=" bg-primary rounded-md text-white w-full xl:px-20 md:px-12 xl:py-22 md:py-10 px-5 py-5 relative">
     <img src="{{asset('assets/img/svg/about.svg')}}" class="xl:w-106 lg:w-80 w-70 absolute top-0 md:top-8 right-3 z-0"/>
     <div>
         <p class="text-white font-extrabold text-xl sm:text-2xl lg:text-4xl xl:text-5xl">Hello John Doe</p>
@@ -34,7 +34,7 @@
             
                     <li class="relative pr-8 lg:pr-14">
                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-2 w-full bg-primary"></div>
+                            <div class="h-2 w-full bg-gray-500"></div>
                         </div>
                         <x-tooltip content="Connect Your Wallet">
                             <div class="relative w-8 h-8 flex items-center justify-center bg-secondary hover:bg-secondary-600 rounded-full" aria-current="step">
@@ -47,10 +47,10 @@
                     <li class="relative pr-8 lg:pr-14">
                         <!-- Upcoming Step -->
                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-2 w-full bg-primary"></div>
+                            <div class="h-2 w-full bg-gray-500"></div>
                         </div>
                         <x-tooltip content="Connect Your Wallet">
-                            <div class="group relative w-8 h-8 flex items-center justify-center bg-primary rounded-full">
+                            <div class="group relative w-8 h-8 flex items-center justify-center bg-gray-500 rounded-full">
                                 <span class="h-2.5 w-2.5 bg-transparent rounded-full" aria-hidden="true"></span>
                                 <span class="sr-only">Step 4</span>
                             </div>
@@ -59,10 +59,10 @@
                 
                     <li class="relative">
                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-2 w-full bg-primary"></div>
+                            <div class="h-2 w-full bg-gray-500"></div>
                         </div>
                         <x-tooltip content="Connect Your Wallet">
-                            <div href="#" class="group relative w-8 h-8 flex items-center justify-center bg-primary rounded-full">
+                            <div href="#" class="group relative w-8 h-8 flex items-center justify-center bg-gray-500 rounded-full">
                                 <span class="h-2.5 w-2.5 bg-transparent rounded-full" aria-hidden="true"></span>
                                 <span class="sr-only">Step 5</span>
                             </div>
