f675822a98ffdae6897c5dde907ff402da9a0859
Bernhard Hörmann <hoermann.b@gmail.com>
2021-12-21 10:44:19 +0100

    ADD: Icon set

--- resources/views/pages/admin/dev/icons.blade.php
+++ resources/views/pages/admin/dev/icons.blade.php
@@ -19,16 +19,14 @@
             @foreach($icons AS $icon)
                 <div class="my-1 px-1 w-1/2 lg:my-4 lg:px-1 lg:w-1/4">
                     <div class="overflow-hidden rounded-lg shadow-lg">
-                        <div>
-                            <x-icon name="{{ $icon->getFilenameWithoutExtension() }}" class="h-8 m-auto" />
-                        </div>
+                        <x-icon name="{{ $icon->getFilenameWithoutExtension() }}" class="h-8 m-auto" />
 
                         <header class="leading-tight p-2 md:p-4">
                             <h1 class="text-lg text-center">
                                 {{ $icon->getFilenameWithoutExtension() }}
                             </h1>
 
-                            <pre class="mt-3 text-xs text-center text-gray-500">&lt;x-icon name="{{ $icon->getFilenameWithoutExtension() }}" class="w-6" /&gt;</pre>
+                            <pre class="mt-3 text-xs text-center text-gray-500">&lt;x-icon name="{{ $icon->getFilenameWithoutExtension() }}" class="h-8" /&gt;</pre>
                         </header>
                     </div>
                 </div>
