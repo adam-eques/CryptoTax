6d2400adb6eae586a6eba9692e64ee3bca612605
Bernhard Hörmann <hoermann.b@gmail.com>
2021-12-21 10:42:51 +0100

    ADD: Icon set

--- resources/views/pages/admin/dev/icons.blade.php
+++ resources/views/pages/admin/dev/icons.blade.php
@@ -7,7 +7,14 @@
 <x-app-layout>
     <x-slot name="title">Icons</x-slot>
 
-    <div class="container my-12 mx-auto px-4 md:px-12">
+    <div class="container  my-12 mx-auto px-4 md:px-12">
+        <h2 class="mb-2 text-xl">Blade Icons Sets</h2>
+        <ul class="ml-6 list-disc text-secondary">
+            <li><a href="https://blade-ui-kit.com/blade-icons?set=9" class="mb-2">Fontawesome Solid</a></li>
+            <li><a href="https://blade-ui-kit.com/blade-icons?set=22" class="mb-2">Cryptoicons</a></li>
+        </ul>
+
+        <h2 class="mt-5 mb-2 text-xl">Custom Icon Set</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
             @foreach($icons AS $icon)
                 <div class="my-1 px-1 w-1/2 lg:my-4 lg:px-1 lg:w-1/4">
