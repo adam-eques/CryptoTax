f22d58ce0ca8b0270e57865fff6b56e165f6768a
Koala-Dev-dream <koalah86@gmail.com>
2021-12-22 07:46:58 -0800

    Add:Icons

--- resources/views/pages/customer/service/service.blade.php
+++ resources/views/pages/customer/service/service.blade.php
@@ -3,13 +3,13 @@
         <div class="mx-auto px-3 xs:px-4 xl:max-w-screen-2xl">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-5">
                 <div class="">
-                    <p class="text-lg">COUPON</p>
-                    <h2 class="text-6xl font-bold mt-3">BIG NEW YEAR OFFER</h2>
-                    <div class="mt-3 border-t-2 border-secondary py-4 flex mx-auto">
-                        <h4 class="text-5xl text-secondary">UPTO <span class="font-bold">65%</span> OFF</h4>
+                    <p class="text-lg">{{ __('COUPON') }}</p>
+                    <h2 class="text-6xl font-bold mt-3">{{ __('Big new year offer') }}</h2>
+                    <div class="mt-3 py-4 flex mx-auto">
+                        <h4 class="text-5xl text-secondary">{{ __('UPTO') }} <span class="font-bold">65%</span> OFF</h4>
                     </div>
-                    <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    <x-button size="xl" class="mt-5 font-extrabold  py-6">Refer and Earn</x-button>
+                    <p class="mt-3">{{ __('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua') }}</p>
+                    <x-button size="xl" class="mt-5 font-extrabold  py-6 px-10">{{ __('Refer and Earn') }}</x-button>
                 </div>
                 <div>
                     <img src="{{ asset('assets/img/svg/service.svg') }}" class="w-full h-full"/>
