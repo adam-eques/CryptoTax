85f931c8db872d0fd27c1f29dde293f74c18b5b9
Koala-Dev-dream <koalah86@gmail.com>
2021-12-21 23:57:45 -0800

    FIX: Code style

--- resources/views/livewire/dashboard/about.blade.php
+++ resources/views/livewire/dashboard/about.blade.php
@@ -1,12 +1,12 @@
 <div class=" bg-bgcolor1 rounded-md text-white w-full xl:px-20 md:px-12 xl:py-22 md:py-10 px-5 py-5 relative">
-    <img src="{{asset('assets/img/svg/about.svg')}}" class="xl:w-106 lg:w-80 w-70 absolute top-0 md:top-8 right-3 z-0"/>
+    <img src="{{asset('assets/img/svg/about.svg')}}" class="xl:w-106 lg:w-80 w-70 absolute top-0 md:top-8 right-3 z-0 hidden md:block"/>
     <div>
-        <p class="text-primary font-extrabold text-xl sm:text-2xl lg:text-3xl xl:text-3xl">Hello John Doe</p>
+        <p class="text-primary font-extrabold text-xl sm:text-2xl lg:text-3xl xl:text-3xl">{{ __("Hello John Doe") }}</p>
         <div class="pt-3">
-            <p class="text-lg font-bold text-primary">Thanks for signing up.</p>
+            <p class="text-lg font-bold text-primary">{{ __("Thanks for signing up.") }}</p>
         </div>
         <div class="py-10">
-            <nav aria-label="Progress">
+            <nav aria-label="Progress" class="hidden sm:block">
                 <ol class="flex items-center">
                     <li class="relative pr-8 lg:pr-16">
                         <div class="absolute inset-0 flex items-center" aria-hidden="true">
--- resources/views/livewire/dashboard/news.blade.php
+++ resources/views/livewire/dashboard/news.blade.php
@@ -1,8 +1,24 @@
+@php
+    $news = [
+        [ 
+            'img' => '', 
+            'title' => 'Cryptocurrency exchanges start blocking accounts', 
+            'content' => 'Data is a real-time snapshot *Data is delayed at least 15 minutes. Global Business and Financial News, Stock Quotes, and Market Data and' 
+        ],
+        [
+            'img' => '', 
+            'title' => 'Cryptocurrency exchanges start blocking accounts', 
+            'content' => 'Data is a real-time snapshot *Data is delayed at least 15 minutes. Global Business and Financial News, Stock Quotes, and Market Data and' 
+        ]
+
+    ]
+@endphp
+
 <div class="bg-white shadow-lg rounded-md p-5 h-full">
     <div class="flex justify-between items-center">
         <div class="flex items-center space-x-2">
             <x-icon name="news" class="w-8 h-8"/>
-            <p class="mr-3 text-lg font-extrabold">Crypto News</p>
+            <p class="mr-3 text-lg font-extrabold">{{ __('Crypto News') }}</p>
         </div>
         <div class="flex items-center space-x-6">
             <button class="border rounded-full w-8 h-8 text-sm flex items-center justify-center bg-white hover:bg-primary text-primary hover:text-white">
@@ -14,20 +30,17 @@
         </div>
     </div>
     <div class="main-carousel pt-5 h-full">
-        <div class="carousel-cell px-1 sm:px-3 w-full md:w-1/2">
-            <div class="h-full w-full">
-                <img src="{{asset('assets/img/jpg/news_1.jpg')}}" class="w-full h-full object-cover"/>
-                <p class="text-base text-primary font-bold">Cryptocurrency exchanges start blocking accounts </p>
-                <p class="text-gray-400 text-sm">Data is a real-time snapshot *Data is delayed at least 15 minutes. Global Business and Financial News, Stock Quotes, and Market Data and</p>
+
+        @foreach ($news as $item)          
+            <div class="carousel-cell px-1 sm:px-3 w-full md:w-1/2">
+                <div class="h-full w-full">
+                    <img src="{{asset('assets/img/jpg/news_1.jpg')}}" class="w-full h-full object-cover"/>
+                    <p class="text-base text-primary font-bold"> {{ __( $item['title'] ) }}</p>
+                    <p class="text-gray-400 text-sm"> {{ __( $item['content'] ) }} </p>
+                </div>
             </div>
-        </div>
-        <div class="carousel-cell px-1 sm:px-3 w-full md:w-1/2">
-            <div class="h-full w-full">
-                <img src="{{asset('assets/img/jpg/news_2.jpg')}}" class="w-full h-full object-cover"/>
-                <p class="text-base text-primary font-bold">Cryptocurrency exchanges start blocking accounts </p>
-                <p class="text-gray-400 text-sm">Data is a real-time snapshot *Data is delayed at least 15 minutes. Global Business and Financial News, Stock Quotes, and Market Data and</p>
-            </div>
-        </div>
+        @endforeach
+        
     </div>
 </div>
 
