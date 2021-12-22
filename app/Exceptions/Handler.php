399810adb0afd22da11405a18e2d4ef0db0c4c3d
Bernhard Hörmann <hoermann.b@gmail.com>
2021-12-22 12:51:33 +0100

    FIX: Exception handler to redirect to login if not authenticated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +39,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $e)
+    {
+        if(method_exists($e, "getStatusCode") && $e->getStatusCode() == Response::HTTP_FORBIDDEN && !auth()->user()) {
+            return redirect("login")->with("message", "Forbidden");
+        }
+
+        return parent::render($request, $e);
     }
 }
