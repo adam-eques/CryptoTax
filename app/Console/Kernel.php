0d20bb1e92e7faa356ac304dfb8c2c8887edace4
Bernhard Hörmann <hoermann.b@gmail.com>
2022-01-06 09:43:40 +0100

    ADD: Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // Prune telescope entries second daily
+        $schedule->command('telescope:prune --hours=48')->daily();
     }
 
     /**
