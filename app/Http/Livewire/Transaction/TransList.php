193bcee1d6771005d8c5558f4d8c445dfcdea70f
Koala-Dev-dream <koalah86@gmail.com>
2022-01-18 07:47:28 -0800

    UPD: account with new logic

--- app/Http/Livewire/Transaction/TransList.php
+++ app/Http/Livewire/Transaction/TransList.php
@@ -4,53 +4,18 @@ namespace App\Http\Livewire\Transaction;
 
 use App\Models\BlockchainTransaction;
 use App\Models\CryptoExchangeTransaction;
-use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Columns\ViewColumn;
-use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
-use Carbon\Carbon;
-use WireUi\Traits\Actions;
 
-class TransList extends Component implements Tables\Contracts\HasTable
+class TransList extends Component
 {
-    use Actions;
-    use Tables\Concerns\InteractsWithTable;
-
-    protected function getTableQuery(): Builder
-    {
-        return CryptoExchangeTransaction::query()
-            ->whereIn("crypto_exchange_account_id", auth()->user()->cryptoExchangeAccounts->pluck("id"));
-    }
-
-    protected function getTableColumns(): array 
-    {
-        return [
-            ViewColumn::make('cryptoExchangeAccount.cryptoExchange.name')
-                ->view('tables.columns.status-switcher')
-                ->searchable()
-                ->label('Exchange'),
-            TextColumn::make('executed_at')
-                ->formatStateUsing(fn($state): string => (new Carbon($state / 1000))->format("Y-m-d H:i:s"))
-                ->sortable(),
-            TextColumn::make('side')
-                ->sortable(),
-            TextColumn::make('amount')
-                ->sortable(),
-            TextColumn::make('cost')
-                ->sortable(),
-            TextColumn::make('fee_currency')
-                ->sortable(),
-            TextColumn::make('fee_cost')
-                ->sortable(),
-            TextColumn::make('fee_rate')
-                ->sortable(),
-        ];
-    }
-
     public function render()
     {
-        return view('livewire.transaction.trans-list');
+        $exchange_transactions = CryptoExchangeTransaction::query()
+            ->whereIn("crypto_exchange_account_id", auth()->user()->cryptoExchangeAccounts->pluck("id"))
+            ->paginate(10);
+        return view('livewire.transaction.trans-list', [
+            "exchange_transactions" => $exchange_transactions
+        ]);
     }
 }
