95e3f0fc84be6ccf757620af9c7e3377763b636c
Koala-Dev-dream <koalah86@gmail.com>
2022-01-21 03:12:12 -0800

    FIX: style of the title on index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,7 +20,7 @@
 
         <div class="w-screen h-screen flex">
             <div class="m-auto">
-                <p class="my-5">MyCryptoTax</p>
+                <p class="my-5 text-xl font-bold">MyCryptoTax</p>
                 <x-button tag="a" href="{{ route('login') }}" class="p-5">LOG IN</x-button>
                 <x-button tag="a" href="{{ route('register') }}" class="p-5">SIGN UP</x-button>
             </div>
