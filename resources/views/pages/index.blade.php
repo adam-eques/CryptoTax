0ffc4d61b1f025324c77201cff12351f988678f8
Koala-Dev-dream <koalah86@gmail.com>
2022-01-21 04:37:48 -0800

    CHG: layout of the index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,29 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        {{-- Meta --}}
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        {{-- Styles --}}
-        <style>[x-cloak] { display: none !important; }</style>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="stylesheet" href="{{ mix('mix/css/app.css') }}">
-        @livewireStyles
-
-        {{-- Scripts --}}
-        <wireui:scripts />
-        <script src="{{ mix('mix/js/app.js') }}" defer></script>
-    </head>
-    <body>
-
-        <div class="w-screen h-screen flex">
-            <div class="m-auto">
-                <p class="my-5 text-xl font-bold">MyCryptoTax</p>
-                <x-button tag="a" href="{{ route('login') }}" class="p-5">LOG IN</x-button>
-                <x-button tag="a" href="{{ route('register') }}" class="p-5">SIGN UP</x-button>
-            </div>
+<x-guest-layout>
+    <div class="w-screen h-screen flex">
+        <div class="m-auto">
+            <p class="my-5 text-xl font-bold">MyCryptoTax</p>
+            <x-button tag="a" href="{{ route('login') }}" class="p-5">LOG IN</x-button>
+            <x-button tag="a" href="{{ route('register') }}" class="p-5">SIGN UP</x-button>
         </div>
-    </body>
-</html>
+    </div>
+</x-guest-layout>
