43e3d2f768ead9c91d35655605265204a39462bd
Koala-Dev-dream <koalah86@gmail.com>
2022-01-24 03:20:59 -0800

    ADD: email verification

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,37 +1,46 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
         </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
-
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        <div  class="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-30 px-3 py-5">
+            <div>
+                <img src="{{ asset('assets/img/svg/email_verify.svg') }}" class="w-auto h-auto"/>
             </div>
-        @endif
-
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-
-                <div>
-                    <x-jet-button type="submit">
-                        {{ __('Resend Verification Email') }}
-                    </x-jet-button>
+            <div class="flex">
+                <div class="m-auto">
+                    <x-jet-authentication-card-logo />
+                    <h2 class="text-3xl font-extrabold my-6">{{ __('Forgot Password?') }}</h2>
+                    <div class="mb-4 text-sm text-gray-600">
+                        {{ __('We have send you an confirmation email to johnjoe7501@gmail.com. please confirm your email address to active your account.') }}
+                    </div>
+            
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                        </div>
+                    @endif
+            
+                    <div class="mt-4 flex items-center justify-between">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
+            
+                            <div>
+                                <x-jet-button type="submit">
+                                    {{ __('Resend confirmation mail') }}
+                                </x-jet-button>
+                            </div>
+                        </form>
+            
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+            
+                            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </form>
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+            </div>
         </div>
     </x-jet-authentication-card>
 </x-guest-layout>
