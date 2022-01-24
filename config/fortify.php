8d2cd13b1ed797041018ff8a7e4cb28ef4098088
Koala-Dev-dream <koalah86@gmail.com>
2022-01-24 01:40:30 -0800

    Forgot Password

--- config/fortify.php
+++ config/fortify.php
@@ -138,7 +138,7 @@ return [
         Features::updateProfileInformation(),
         Features::updatePasswords(),
         Features::twoFactorAuthentication([
-            'confirmPassword' => false,
+            'confirmPassword' => true,
         ]),
     ],
 
