8610cd1b92fcbcf382c1d630f669658c4bca325c
Koala-Dev-dream <koalah86@gmail.com>
2022-01-25 01:46:38 -0800

    ADD: require exchange UI in Setting

---
+++ resources/views/livewire/tax-setting/exchange-blockchain.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="mt-7">
+        <x-jet-label>{{ __('Coin Name') }}</x-jet-label>
+        <x-jet-input type="text" class="w-full mt-4" placeholder="Bitcoin (BTC )"/>
+    </div>
+    <div class="mt-7">
+        <x-jet-label>{{ __('Wallet Address') }}</x-jet-label>
+        <x-jet-input type="text" class="w-full mt-4" placeholder="19rxWcjug44Xft1T1Ai11ptDZr94wEdRTz"/>
+    </div>
+    <div class="mt-7">
+        <p class="text-primary">{{ __('Your email address.') }}</p>
+        <x-jet-label class="py-3">{{ __('We will let you know when the integration is ready or we need more details on it.') }}</x-jet-label>
+        <x-jet-input type="email" class="w-full mt-4" placeholder="email@gmail.com"/>
+    </div>
+    <div class="mt-7">
+        <p class="text-primary">{{ __('Comments') }}</p>
+        <x-jet-label class="py-3">{{ __('Please add any more information that may be helpful.') }}</x-jet-label>
+        <textarea rows="6" class="w-full border shadow rounded-sm border-gray-300"></textarea>
+    </div>
+    <x-jet-button class="mt-7">{{ __('Submit') }}</x-jet-button>
+</div>
+</div>
--- resources/views/livewire/tax-setting/exchange.blade.php
+++ resources/views/livewire/tax-setting/exchange.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="bg-gray-100 w-full px-10 py-6 rounded">
+        <span class="text-xl font-extrabold">{{ __('Request Exchange') }}</span>
+    </div>
+    <div class="px-5 py-5 md:px-10 md:py-10">
+        <div>
+            <x-jet-label>{{ __('Exchange Type') }}</x-jet-label>
+            <select class="w-full mt-4 py-4 border border-gray-300 rounded-sm shadow" id="type" wire:model="type">
+                <option value="1" selected>{{ __('CSV') }}</option>
+                <option value="2">{{ __('API') }}</option>
+                <option value="3">{{ __('Blockchain') }}</option>
+            </select>
+        </div>
+        <div class="mt-7">
+            @switch($type)
+                @case(1)
+                    @livewire('tax-setting.exchange-csv')
+                    @break
+                @case(2)
+                    @livewire('tax-setting.exchange-api')
+                    @break
+                @case(3)
+                    @livewire('tax-setting.exchange-blockchain')
+                    @break
+                @default
+                    
+            @endswitch
+        </div>
+    </div>
 </div>
