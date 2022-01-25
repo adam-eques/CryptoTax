30b6490c772b82100f7ac310252034b4271616a1
Bernhard Hörmann <hoermann.b@gmail.com>
2022-01-25 15:02:57 +0100

    FIX: missing namespace

--- resources/views/components/tax-setting-list.blade.php
+++ resources/views/components/tax-setting-list.blade.php
@@ -19,15 +19,15 @@
     ]
 @endphp
 
-<div class="divide-y hidden md:block">  
+<div class="divide-y hidden md:block">
     @foreach ($list as $item)
-        <a href="{{ route('tax-setting', ['category' => $item['route']]) }}">
+        <a href="{{ route('customer.tax-setting', ['category' => $item['route']]) }}">
             <div class="flex items-center justify-between w-full text-left py-6 px-8 @if($active == $item['route']) bg-gray-100 @endif ">
                 <div class="flex items-center">
                     <x-icon name="{{$item['icon']}}" class="w-6 h-auto mr-6"/>
                     {{ __($item['name']) }}
                 </div>
-                @if ($active == $item['route'])                    
+                @if ($active == $item['route'])
                     <x-icon name="arrow-right" class="w-6"/>
                 @endif
             </div>
@@ -37,25 +37,25 @@
 <div class="block md:hidden">
     <div class="relative" x-data="{open:false}">
         <button
-            @click="open = true" 
+            @click="open = true"
             class="py-3 h-full w-full flex justify-start px-5 text-gray-600"
         >
             <x-icon name="list" class="h-8" />
         </button>
-        <div 
+        <div
             class="origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none  overflow-auto"
             x-show="open" @click.away="open=false"
             x-transition:enter-start="transition ease-in duration-3000"
         >
-            <div class="h-80">                
+            <div class="h-80">
                 @foreach ($list as $item)
-                    <a href="{{ route('tax-setting', ['category' => $item['route']]) }}">
+                    <a href="{{ route('customer.tax-setting', ['category' => $item['route']]) }}">
                         <div class="flex items-center justify-between w-full text-left py-6 px-8 @if($active == $item['route']) bg-gray-100 @endif ">
                             <div class="flex items-center">
                                 <x-icon name="{{$item['icon']}}" class="w-6 h-auto mr-6"/>
                                 {{ __($item['name']) }}
                             </div>
-                            @if ($active == $item['route'])                    
+                            @if ($active == $item['route'])
                                 <x-icon name="arrow-right" class="w-6"/>
                             @endif
                         </div>
