60851c9d9f742f261da21e139a664847128cf687
Koala-Dev-dream <koalah86@gmail.com>
2022-01-26 05:11:27 -0800

    UPD: tax setting

--- app/Http/Livewire/UserSetting/Tax.php
+++ app/Http/Livewire/UserSetting/Tax.php
@@ -4,10 +4,65 @@ namespace App\Http\Livewire\UserSetting;
 
 use Livewire\Component;
 
+use App\Models\Timezone;
+use App\Models\TaxCountry;
+use App\Models\TaxCurrency;
+use App\Models\TaxCostModel;
+use App\Models\User;
+
+use WireUi\Traits\Actions;
+
 class Tax extends Component
 {
+    use Actions;
+
+    public?int $timezone_id = null;
+    public?int $tax_country_id = null;
+    public?int $tax_year = null;
+    public?int $tax_currency_id = null;
+    public?int $tax_cost_model_id = null;
+
+    public function __construct()
+    {
+        $this->timezone_id = auth()->user()->timezone ? auth()->user()->timezone->id : 0;
+        $this->tax_country_id = auth()->user()->taxCountry? auth()->user()->taxCountry->id : 0;
+        $this->tax_year = auth()->user()->tax_year?auth()->user()->tax_year:0;
+        $this->tax_currency_id = auth()->user()->taxCurrency?auth()->user()->taxCurrency->id:0;
+        $this->tax_cost_model_id = auth()->user()->taxCostModel?auth()->user()->taxCostModel->id:0;
+    }
+
+    public function save_tax_setting_data ()
+    {
+        $user = User::query()->firstWhere('id', auth()->user()->id);
+        if ($user) {
+            if($this->timezone_id != 0) $user-> timezone_id = $this->timezone_id;
+            if($this->tax_country_id != 0) $user-> tax_country_id = $this->tax_country_id;
+            if($this->tax_year != 0) $user-> tax_year = $this->tax_year;
+            if($this->tax_currency_id != 0) $user-> tax_currency_id = $this->tax_currency_id;
+            if($this->tax_cost_model_id != 0) $user-> tax_cost_model_id = $this->tax_cost_model_id;
+            $user->save();
+            $this->notification()->success(
+                $title = __('Successfully Saved'),
+                $description = ''
+            );
+
+        }
+    }
+
     public function render()
     {
-        return view('livewire.user-setting.tax');
+        $timezone = Timezone::query() -> get();
+        $tax_countries = TaxCountry::query() -> get();
+        $tax_years = [2018, 2019, 2020, 2021, 2022];
+        $basic_currency = TaxCurrency::query() -> get();
+        $basic_cost_method = TaxCostModel::query() -> get();
+
+        return view('livewire.user-setting.tax', [
+            'timezone' => $timezone,
+            'tax_countries' => $tax_countries,
+            'tax_years' => $tax_years,
+            'basic_currency' => $basic_currency,
+            'basic_cost_method' => $basic_cost_method
+        ]);
     }
 }
