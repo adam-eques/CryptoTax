026759acbc4b7b65b85c80dc35f631ffe0cdc0f1
Koala-Dev-dream <koalah86@gmail.com>
2022-02-02 05:44:09 -0800

    fix: dasign of landingpage

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -1,26 +1,30 @@
 <x-guest-layout>
-    <div class="w-full bg-white relative">
-        <img src="{{ asset("assets/img/svg/account_pattern.svg") }}" class="absolute -left-3 -top-2 z-0 w-full h-auto"/>
-        <x-landing-nav for="customer" logo="white"/>
-        <div class="mx-auto px-3 xs:px-4 xl:max-w-screen-2xl lg:px-5 relative">
-
-            {{-- Hero section --}}
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-                <div class=" flex items-center justify-start order-2 sm:order-1">
-                    <div class="my-auto relative">
-                        <h5 class="text-primary sm:text-white text-xl">{{ __('Duis consectetur feugiat auctor') }}</h5>
-                        <h2 class="text-primary sm:text-white xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-bold mt-7">{{ __('Add Your Accounts') }}</p>
-                        <h5 class="text-primary sm:text-white text-lg mt-6">{{ __('Use our cryptocurrency tax software to easily track your trades, see your profits, and never overpay on your crypto taxes again.') }}</h5>
-                        <x-button variant="secondary" size="lg" class="mt-6 border-0 tracking-tight font-bold">{{ __('Add Accounts') }}</x-button>
+    <div class="w-full bg-white">
+        <div class="relative">
+            <div class="w-full absolute right-0 top-0">
+                <img src="{{ asset("assets/img/svg/account_banner.svg") }}" class="w-full object-cover lg:block md:hidden"/>
+            </div>
+            <x-landing-nav for="customer" logo="white"/>
+            <x-container class="py-10 sm:py-28 relative">
+                {{-- Hero section --}}
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+                    <div class=" flex items-center justify-start order-2 sm:order-1">
+                        <div class="my-auto relative">
+                            <h5 class="text-primary lg:text-white text-xl">{{ __('Duis consectetur feugiat auctor') }}</h5>
+                            <h2 class="text-primary lg:text-white xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-bold mt-7">{{ __('Add Your Accounts') }}</p>
+                            <h5 class="text-primary lg:text-white text-lg mt-6">{{ __('Use our cryptocurrency tax software to easily track your trades, see your profits, and never overpay on your crypto taxes again.') }}</h5>
+                            <x-button variant="secondary" size="lg" class="mt-6 border-0 tracking-tight font-bold">{{ __('Add Accounts') }}</x-button>
+                        </div>
+                    </div>
+                    <div class="order-1 sm:order-2 py-10 sm:py-0">
+                        <img src="{{ asset('assets/img/svg/account_hero.svg') }}" class="w-full" />
                     </div>
                 </div>
-                <div class="order-1 sm:order-2 mt-26">
-                    <img src="{{ asset('assets/img/svg/account_hero.svg') }}" class="w-full" />
-                </div>
-            </div>
-
+            </x-container>
+        </div>
+        <x-container>    
             {{--  --}}
-            <div class="text-center sm:mt-28 mt-8">
+            <div class="text-center sm:mt-10 mt-8">
                 <p class="text-secondary font-semibold">{{ __('Add Account') }}</p>
                 <h3 class="font-bold text-xl md:text-3xl lg:text-4xl xl:text-5xl mt-4">{{ __('Simple and Easy, Add Your Account') }}</h3>
                 <p class="mt-5">{{ __('Duis consectetur feugiat auctor. Morbi nec enim luctus, feugiat arcu id, ultricies ante. Duis vel massa eleifend, porta ') }}</p>
@@ -51,7 +55,7 @@
                 <p class="mt-2">{{ __('est non, feugiat metus. Cras ante massa, tincidunt nec lobortis quis') }}</p>
                 <img src="{{ asset('assets/img/svg/account_3.svg') }}" class="w-full mt-8"/>
             </div>
-        </div>
+        </x-container>
        <x-footer-get-start/>
     </div>
     <x-footer/>
