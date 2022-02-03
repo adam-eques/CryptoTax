ad6cdcc5bfa777342aadc7b321f826d39538fc97
Koala-Dev-dream <koalah86@gmail.com>
2022-02-03 06:42:40 -0800

    add: contact us page UI

---
+++ app/Http/Livewire/SubPages/ContactUs/EmailContact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\SubPages\ContactUs;
+
+use Livewire\Component;
+
+class EmailContact extends Component
+{
+    public function render()
+    {
+        return view('livewire.sub-pages.contact-us.email-contact');
+    }
+}
---
+++ app/Http/Livewire/SubPages/Faqs/FaqsList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\SubPages\Faqs;
+
+use Livewire\Component;
+
+class FaqsList extends Component
+{
+    public function render()
+    {
+        return view('livewire.sub-pages.faqs.faqs-list');
+    }
+}
