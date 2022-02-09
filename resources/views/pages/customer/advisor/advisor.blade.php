3b825de8e2260108d12bb387d1b4c5296d8244e6
Koala-Dev-dream <koalah86@gmail.com>
2022-02-09 08:18:05 -0800

    upd: advisor page ui

--- resources/views/pages/customer/advisor/advisor.blade.php
+++ resources/views/pages/customer/advisor/advisor.blade.php
@@ -1,33 +1,52 @@
 <x-app-layout>
-    @livewire('customer.advisor.advisor-banner')
-    <div class="mx-auto my-5 px-3 xs:px-4 xl:max-w-screen-2xl lg:px-5 py-5">
-        @livewire('customer.advisor.advisor-list')
-    </div>
-    <div class="bg-secondary-100">
-        @livewire('customer.advisor.advisor-carousel')
-    </div>
-    <div class="mx-auto my-5 px-3 xs:px-4 xl:max-w-screen-2xl lg:px-5 py-5">
-        <div class="grid grid-cols-1 md:grid-cols-4 md:gap-10 gap-0">
-            <div class="w-full col-span-2">
-                <div class="px-5 md:px-10 py-10"><span class="text-2xl font-bold">areas of responsibility of the financial advisor</span></div>
-                <div class="px-5 md:px-10">
-                    <span class="md:text-lg text-md text-gray-700">The term "financial advisor", unlike that of the insurance advisor, is not protected by law. The financial advisor advises his clients on investments, loans and insurance. This advice is provided either on a case-by-case basis or as part of systematic financial planning.<br/><br/><br/>
-                    Synonymously used terms for financial advisors are investment advisers, free or independent investment brokers, Financial Advisor or Financial Advisor . A distinction is made between two categories of financial advisors: financial advisors who are allowed to buy and sell financial services in accordance with the provisions of the German Banking Act (KWG) and financial advisors who are only allowed to sell investment funds, closed-end funds, financing and real estate in accordance with the rules of the Commercial Code (GewO). </span>
+    {{-- Hero section --}}
+    <div class="bg-primary w-full">
+        <x-container class="text-white pt-8">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div class="flex h-full order-2 sm:order-1 text-center sm:text-left">
+                    <div class="my-auto">
+                        <h2 class="text-2xl sm:text-4xl font-bold">{{ __("HIRE THE BEST FINANCIAL ADVISORS") }}</h2>
+                        <p class="font-semibold mt-2">{{ __('The top financial advisors in Germany with ratings') }}</p>
+                        <div class="grid grid-cols-1 sm:grid-cols-2 gap-5 sm:gap-10 py-8" x-data="{total_value:50}">
+                            <input class="w-full px-7 py-5 border-none rounded-md outline-none ring-0 text-primary" placeholder="Advisor Name"/>
+                            <input class="w-full px-7 py-5 border-none rounded-md outline-none ring-0 text-primary" placeholder="Zip or city"/>
+                            <select class="w-full px-7 py-5 border-none rounded-md outline-none ring-0 text-primary">
+                                <option disabled selected>{{ __('All Consulting Topics') }}</option>
+                                <option></option>
+                            </select>
+                            <select class="w-full px-7 py-5 border-none rounded-md outline-none ring-0 text-primary">
+                                <option disabled selected>{{ __('Financial Advisor') }}</option>
+                                <option></option>
+                            </select>
+                            <div>
+                                <p class="text-sm text-gray-700">{{ __('Distance') }}</p>
+                                <input class="w-full bg-danger mt-6" type="range" x-model="total_value" min="0" max="100" step="1">
+                            </div>
+                            <button class="w-full px-7 py-5 border-none rounded-md outline-none ring-0 bg-secondary font-semibold">{{ __('Find a Advisor') }}</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="order-1 sm:order-2">
+                    <img src="{{asset('assets/img/svg/advisor_banner.svg')}}"/>
                 </div>
             </div>
-            <div class="w-full col-span-2">
-                <img src="{{asset('assets/img/svg/advisor.svg')}}" class="w-full"/>
-            </div>
-        </div>
-        <div>
-            <div class="px-5 md:px-10 py-10">
-                <span class="text-2xl font-bold">Financial advisor with a license in accordance with the trade regulations (GewO)</span>
-            </div>
-            <div class="px-5 md:px-10">
-                <span class="md:text-lg text-md text-gray-700">
-                    The financial advisor, with permission according to § 34f GewO, does not have a license under the Banking Act and is therefore limited in investment advice to investment funds. He may not make any statements about investments such as stocks or fixed-income securities. Financial advisors who advise on the subject of mortgage lending, however, require a permit according to §34i GewO. This paragraph was only newly created in 2016. Brokers who advise on the subject of insurance and sell corresponding products are subject to the provisions of §34d GewO (insurance broker) and §34e (insurance consultant). The brokerage of real estate is regulated in §34c GewO. In addition, there are a number of certifications, e.g. Certified DEFINO Consultant or Certified Financial Planner® , which are awarded on the basis of expert examinations and defined advisory rules. Financial advisors have brokerage agreements with a very different number of product providers and are mostly self-employed. As a rule, financial advisors receive contract and / or portfolio commissions from the product providers for their sales and support services. 
-                </span>
-            </div>
-        </div>
+        </x-container>
     </div>
+
+    <x-container class="bg-white mt-16 border">
+        <div class="py-6 sm:py-12 px-0 sm:px-6">
+            <h3 class="text-2xl font-bold">{{ __('TOP 100 ADVISORS') }}</h3>
+            <p class="text-base mt-4 pb-6 border-b">{{ __('Clients Rate Financial Advisors ') }} <span class="text-gray-400 text-sm">{{ __('based on 106,079 client reviews') }}</span> </p>
+            <p class="text-gray-400 text-sm mt-7">{{ __('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusant doloremque laudantium totam rem aperiam, eaque ipsa quae ab illo inventore verit quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspe aut odit aut fugit sed quia consequuntur') }}</p>
+            <h3 class="text-2xl font-bold mt-10 mb-8">{{ __('Recommended for you') }}</h3>
+            @livewire('customer.advisor.recommended')
+            <div class="mt-12 border-b"></div>
+            <h3 class="mt-12 text-2xl font-bold">{{ __('Search for your results') }}</h3>
+            <div class="mt-8">
+                @livewire('customer.advisor.advisor-list')
+            </div>
+        </div>
+    </x-container>
+
+
 </x-app-layout>
