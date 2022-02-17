7c2a7028dbdd0203d1ea1521c7db3e3ffb472778
Koala-Dev-dream <koalah86@gmail.com>
2022-02-16 17:27:13 -0800

    upd: transaction api in transaction page

--- resources/views/livewire/customer/transaction/transaction-list.blade.php
+++ resources/views/livewire/customer/transaction/transaction-list.blade.php
@@ -1,52 +1,4 @@
-@php
-    $transactions = [
-        [
-            'icon' => '',
-            'name' => 'Bitpanda BTC wallet',
-            'amount' => '- 0.003321456 BTC',
-            'balance' => '0.001311 BTC',
-            'date' => 'FEB 26, 2009  00:12.39',
-            'zone' => 'UTC Transfer',
-            'type' => 'send'
-        ],
-        [
-            'icon' => '',
-            'name' => 'Bitpanda BTC wallet',
-            'amount' => '- 0.003321456 BTC',
-            'balance' => '0.001311 BTC',
-            'date' => 'FEB 26, 2009  00:12.39',
-            'zone' => 'UTC Transfer',
-            'type' => 'send'
-        ],
-        [
-            'icon' => '',
-            'name' => 'Bitpanda BTC wallet',
-            'amount' => '- 0.003321456 BTC',
-            'balance' => '0.001311 BTC',
-            'date' => 'FEB 26, 2009  00:12.39',
-            'zone' => 'UTC Transfer',
-            'type' => 'send'
-        ],
-        [
-            'icon' => '',
-            'name' => 'Bitpanda BTC wallet',
-            'amount' => '- 0.003321456 BTC',
-            'balance' => '0.001311 BTC',
-            'date' => 'FEB 26, 2009  00:12.39',
-            'zone' => 'UTC Transfer',
-            'type' => 'send'
-        ],
-        [
-            'icon' => '',
-            'name' => 'Bitpanda BTC wallet',
-            'amount' => '- 0.003321456 BTC',
-            'balance' => '0.001311 BTC',
-            'date' => 'FEB 26, 2009  00:12.39',
-            'zone' => 'UTC Transfer',
-            'type' => 'send'
-        ],
-    ]
-@endphp
+
 <div>
     <div class="bg-gray-100 border rounded-sm p-4">
         <div class="grid grid-cols-1 md:grid-cols-12 gap-x-0 md:gap-x-2 gap-y-2 md:gap-y-0">
@@ -100,29 +52,45 @@
             <div class="grid grid-cols-3 p-9 border min-w-max">
                 <div class="flex justify-start items-center">
                     <input type="checkbox">
-                    <x-icon name="kucoin-token" class="w-18 h-18 ml-7"/>
+                    <x-icon name="coins.{{$transaction->cryptoExchangeAccount->cryptoExchange->getName()}}" class="w-18 h-18 ml-7"/>
                     <div class="w-full ml-7 space-y-1">
-                        <p class="text-gray-500">{{ $transaction->id }} </p>
-                        <p class="font-bold text-lg"> </p>
-                        <p class="text-gray-500">View Transactions</p>
-                        <p class="text-gray-500"> </p>
+                        <p class="text-gray-500">{{$transaction->cryptoExchangeAccount->cryptoExchange->getName()}} {{ explode('/', $transaction->symbol)[0] }} {{ __('Wallet') }} </p>
+                        <p class="font-semibold text-lg"> {{ $transaction->amount }} {{ explode('/', $transaction->symbol)[0] }} </p>
+                        <p class="text-gray-500">{{ __('View Transactions') }}</p>
+                        <p class="text-gray-500">{{ $transaction->fee_cost }} {{ $transaction->fee_currency }}</p>
                     </div>
                 </div>
                 <div class="flex justify-start items-center">
-                    <div class="px-2 py-4 bg-gray-100 border rounded-lg">
-                        <x-icon name="fas-arrow-right" class="w-8 h-5 text-primary"/>
-                    </div>
-                    <div class="w-full ml-7 space-y-1">
-                        <p class="font-bold text-lg"></p>
-                        <p class="text-gray-500"></p>
-                        <x-badge variant="danger" type="square"></x-badge>
-                    </div>
+                    @switch($transaction->side)
+                        @case('sell')                                
+                            <div class="px-3 py-5 bg-gray-100 rounded-lg">
+                                <x-icon name="arrow-right" class="w-6 text-primary"/>
+                            </div>
+                            <div class="w-full ml-7 space-y-1">
+                                <p class="font-semibold text-lg">{{ date("M d, Y H:i:s", $transaction->executed_at/1000) }}</p>
+                                <p class="text-gray-500">{{ __('UTC Transfer') }}</p>
+                                <x-badge variant="danger" type="square">{{ $transaction->side }}</x-badge>
+                            </div>
+                            @break
+                        @case('buy')
+                            <div class="px-3 py-5 bg-gray-100 rounded-lg">
+                                <x-icon name="arrow-right" class="w-6 text-primary rotate-180"/>
+                            </div>
+                            <div class="w-full ml-7 space-y-1">
+                                <p class="font-semibold text-lg">{{ date("M d, Y H:i:s", $transaction->executed_at/1000) }}</p>
+                                <p class="text-gray-500">{{ __('UTC Transfer') }}</p>
+                                <x-badge variant="success" type="square">{{ $transaction->side }}</x-badge>
+                            </div>
+                            @break
+                        @default
+                    @endswitch
+                   
                 </div>
                 <div class="flex justify-between items-center">
-                    <x-icon name="kucoin-token" class="w-18 h-18 ml-7"/>
+                    <x-icon name="coins.{{$transaction->cryptoExchangeAccount->cryptoExchange->getName()}}" class="w-18 h-18 ml-7"/>
                     <div class="w-full ml-7 space-y-1">
-                        <p class="text-gray-500"> </p>
-                        <p class="font-bold text-lg"> </p>
+                        <p class="text-gray-500">{{$transaction->cryptoExchangeAccount->cryptoExchange->getName()}} {{ explode('/', $transaction->symbol)[1] }} {{ __('Wallet') }}</p>
+                        <p class="font-semibold text-lg">{{ $transaction->cost }} {{ explode('/', $transaction->symbol)[1] }}</p>
                     </div>
                     <div>
                         <button class="rounded-full w-6 h-6 bg-gray-100 border flex justify-center items-center">
@@ -133,41 +101,7 @@
             </div>
         @endforeach
     </div>
-    <div>
-        <div class="max-w-8xl mx-auto container py-10">
-            <ul class="flex justify-center items-center">
-                <li>
-                    <span tabindex="0" aria-label="backward" role="button" class="focus:ring-2 focus:ring-offset-2 focus:ring-primary p-1 flex rounded transition duration-150 ease-in-out text-base leading-tight font-bold text-gray-500 hover:text-primary focus:outline-none mr-1 sm:mr-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <polyline points="15 6 9 12 15 18" />
-                        </svg>
-                    </span>
-                </li>
-                <li>
-                    <span tabindex="0" class="focus:bg-primary focus:text-white flex text-primary bg-white hover:bg-primary hover:text-white text-base leading-tight font-bold cursor-pointer shadow transition duration-150 ease-in-out mx-2 sm:mx-4 rounded px-4 py-2 focus:outline-none">1</span>
-                </li>
-                <li>
-                    <span tabindex="0" class="focus:bg-primary focus:text-white flex text-primary bg-white hover:bg-primary hover:text-white text-base leading-tight font-bold cursor-pointer shadow transition duration-150 ease-in-out mx-2 sm:mx-4 rounded px-4 py-2 focus:outline-none">2</span>
-                </li>
-                <li>
-                    <span tabindex="0" class="focus:bg-primary focus:text-white flex text-primary bg-white hover:bg-primary hover:text-white rounded transition duration-150 ease-in-out text-base leading-tight font-bold shadow px-4 py-2 focus:outline-none">...</span>
-                </li>
-                <li>
-                    <span tabindex="0" class="focus:bg-primary focus:text-white flex text-primary bg-white hover:bg-primary hover:text-white text-base leading-tight font-bold cursor-pointer transition shadow duration-150 ease-in-out mx-2 sm:mx-4 rounded px-4 py-2 focus:outline-none">6</span>
-                </li>
-                <li>
-                    <span tabindex="0" class="focus:bg-primary focus:text-white flex text-primary bg-white hover:bg-primary hover:text-white text-base leading-tight font-bold cursor-pointer transition shadow duration-150 ease-in-out mx-2 sm:mx-4 rounded px-4 py-2 focus:outline-none">7</span>
-                </li>
-                <li>
-                    <span tabindex="0" aria-label="forward" role="button" class="focus:ring-2 focus:ring-offset-2 focus:ring-primary flex rounded transition duration-150 ease-in-out text-base leading-tight font-bold text-gray-500 hover:text-primary p-1 focus:outline-none ml-1 sm:ml-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <polyline points="9 6 15 12 9 18" />
-                        </svg>
-                    </span>
-                </li>
-            </ul>
-        </div>
+    <div class="mt-16 mb-8">
+        {{$exchange_transactions->links()}}
     </div>
 </div>
