887b974eca59f2a34c23140d1a00b608cd1131a7
Koala-Dev-dream <koalah86@gmail.com>
2022-02-18 16:51:38 -0800

    fix: transaction Icons

--- app/Http/Livewire/Customer/Transaction/TransactionList.php
+++ app/Http/Livewire/Customer/Transaction/TransactionList.php
@@ -15,12 +15,37 @@ class TransactionList extends Component
 
     public function render()
     {
+        $filter = [
+            'order' => [ 
+                ['label' =>'Newest', 'value' => 'newest'], 
+                ['label' => 'Oldest', 'value' => 'oldest'] 
+            ],
+            'type' => [ 
+                ['label' => 'Transfer', 'value' => 'transfer'], 
+                ['label' => 'Trade', 'value' => 'trade'], 
+                ['label' => 'Bought', 'value' => 'buy'], 
+                ['label' => 'Sold', 'value' => 'sell'], 
+                ['label' => 'Received', 'value' => 'receive'], 
+                ['label' => 'Sent', 'value' => 'send'], 
+                ['label' => 'Mint', 'value' => 'mint'] 
+            ],
+            'category' => [
+                ['label' => 'Kucoin', 'value' => 'kucoin'],
+                ['label' => 'Solana', 'value' => 'Solana']
+            ],
+            'exchange' => [
+                ['label' => 'Kucoin', 'value' => 'kucoin'],
+                ['label' => 'Solana', 'value' => 'Solana']
+            ]
+        ];
+
         $exchange_transactions = CryptoExchangeTransaction::query()
-        ->whereIn("crypto_exchange_account_id", auth()->user()->cryptoExchangeAccounts->pluck("id"))
-        ->paginate(10);
+            ->whereIn("crypto_exchange_account_id", auth()->user()->cryptoExchangeAccounts->pluck("id"))
+            ->paginate(10);
 
         return view('livewire.customer.transaction.transaction-list', [
-            "exchange_transactions" => $exchange_transactions
+            "exchange_transactions" => $exchange_transactions,
+            'filter' => $filter
         ]);
     }
 }
--- resources/views/components/account-assets-item.blade.php
+++ resources/views/components/account-assets-item.blade.php
@@ -2,19 +2,19 @@
     <div class="col-span-3">
         <div class="flex items-center space-x-3">
             <div class="w-14">
-                <x-icon name="{{'coins.' . str_replace(' ', '-',strtolower( $asset->cryptoCurrency()->first()->getName()))}}" class="w-14 h-14"/>
+                <x-icon name="{{ 'coins.' . $asset->cryptoCurrency->short_name }}" class="w-14 h-14"/>
             </div>
             <div>                                                    
-                <p class="font-bold truncate">{{$asset->cryptoCurrency()->first()->getName()}} Wallet</p>
+                <p class="font-bold truncate">{{$asset->cryptoCurrency->getName()}} Wallet</p>
                 <p class="text-gray-400">{{ 11 }} {{__('Transactions')}}</p>
             </div>
         </div>
     </div>
     <div class="col-span-2 text-right">
         <p class="font-bold">${{ moneyFormat($asset->convertTo(), 2) }}</p>
-        <p class="text-gray-400">{{ moneyFormat($asset->balance, 10) }} {{$asset->cryptoCurrency()->get()[0]->short_name}}</p>
+        <p class="text-gray-400">{{ moneyFormat($asset->balance, 10) }} {{$asset->cryptoCurrency->short_name}}</p>
     </div>
     <div class="col-span-2">
-        <x-button tag="a" href="{{route('customer.transactions')}}" variant="white" class="justify-center w-full text-center rounded-full border-primary">{{ __('View Transaction') }}</x-button>
+        <x-button tag="a" href="{{route('customer.transactions', ['asset' => $asset->cryptoCurrency->getName()])}}" variant="white" class="justify-center w-full text-center rounded-full border-primary">{{ __('View Transaction') }}</x-button>
     </div>
 </div>
