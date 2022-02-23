29e09dc19e1788e5325c16b14b6cd44cc1066569
Koala-Dev-dream <koalah86@gmail.com>
2022-02-23 00:04:58 -0800

    fix: profile setting

--- app/Http/Livewire/UserSetting/Profile.php
+++ app/Http/Livewire/UserSetting/Profile.php
@@ -3,9 +3,32 @@
 namespace App\Http\Livewire\UserSetting;
 
 use Livewire\Component;
+use App\Models\Models\User;
 
 class Profile extends Component
 {
+    public?string $name = null;
+    public?string $phone = null;
+
+    public function mount()
+    {
+
+        $this->name = auth()->user()->name;
+        $this->phone = '';
+
+    }
+
+    public function update_userprofile()
+    {
+        $user = auth()->user();
+
+        if($user)
+        {
+            $user->name = $this->name;
+            $user->save();
+        }
+    }
+
     public function render()
     {
         return view('livewire.user-setting.profile');
