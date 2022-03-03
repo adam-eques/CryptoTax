83a4a71c4baebb5268de2016cee281047390e6c5
Koala-Dev-dream <koalah86@gmail.com>
2022-03-03 01:04:38 -0800

    upd: chart on portfolio

--- resources/views/livewire/customer/portfolio/line-chart.blade.php
+++ resources/views/livewire/customer/portfolio/line-chart.blade.php
@@ -9,18 +9,13 @@
             </div>
         </div>
         <div class="flex items-center space-x-2">
-            @php
-                $buttons = [
-                    [ 'name' => '1D' ],
-                    [ 'name' => '1W' ],
-                    [ 'name' => '1M' ],
-                    [ 'name' => '3M' ],
-                    [ 'name' => '1Y' ],
-                    [ 'name' => 'ALL' ],
-                ]
-            @endphp
             @foreach ($buttons as $button)                
-                <button class="px-3 py-1 font-bold bg-white rounded-md text-primary hover:text-white hover:bg-primary">{{ $button['name'] }}</button>
+                <button 
+                    class="px-3 py-1 font-bold rounded-md hover:text-white hover:bg-primary @if($button['id'] == $selected) bg-primary text-white @else text-primary @endif"
+                    wire:click="get_selected({{ $button['id'] }})""
+                >
+                    {{ $button['name'] }}
+                </button>
             @endforeach
         </div>
     </div>
@@ -47,11 +42,11 @@
             colors: ["#796cff", "#a69dff"],
             series: [
                 {
-                    data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+                    data: @json($line_data['value'])
                 },
-                {
-                    data: [2.6, 3.3, 3.8, 2.7, 3.6, 3.9, 5.2, 5.9]
-                }
+                // {
+                //     data: [2.6, 3.3, 3.8, 2.7, 3.6, 3.9, 5.2, 5.9]
+                // }
             ],
             stroke: {
                 curve: 'smooth',
@@ -73,7 +68,7 @@
                 }
             },
             xaxis: {
-                categories: ["Sep 2019", "Oct 2019", "Nov 2019", "Dec 2019", "Jan 2020", "Feb 2020", "Mar 2020", "Apr 2020"]
+                categories: @json($line_data['label'])
             },
             yaxis: {
                 axisTicks: {
@@ -95,6 +90,19 @@
         };
         const chart = new ApexCharts(document.getElementById(`chart`), options);
         chart.render();
+
+        document.addEventListener('livewire:load', () => {
+            @this.on(`refresh-line-chart`, (chartData) => {
+                chart.updateOptions({
+                    xaxis: {
+                        categories: chartData.line_data.label
+                    },
+                    series: [{
+                        data: chartData.line_data.value
+                    }],
+                });
+            })
+        })
     }());
 </script>
 @endpush
--- resources/views/livewire/customer/transaction/overview.blade.php
+++ resources/views/livewire/customer/transaction/overview.blade.php
@@ -28,15 +28,24 @@ $overview_data = [
 ]    
 @endphp
 
-
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-0 sm:gap-x-4 gap-y-4 sm:gap-y-0">
-    @foreach ($overview_data as $item)
-        <div class="flex items-start justify-between w-full p-8 border rounded-lg">
-            <div>
-                <p> {{ $item['type'] }}</p>
-                <p class="mt-1 text-3xl font-bold">{{ $item['amount'] }}</p>
+<div>
+    <x-customers.customer-header-bar icon="grommet-transaction" name="Transactions">
+        <x-button variant="white" class="my-2 border-primary">{{ __('Download CSV') }}</x-button>
+        <x-button variant="white" class="border-primary">{{ __('Add transaction') }}</x-button>
+        <x-button tag="a" href="{{ route('customer.account.new') }}">
+            <x-icon name="bx-add-to-queue" class="w-5 h-5 mr-2"/>
+            {{ __('Add Account') }}
+        </x-button>
+    </x-customers.customer-header-bar>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-0 sm:gap-x-4 gap-y-4 sm:gap-y-0 my-9">
+        @foreach ($overview_data as $item)
+            <div class="flex items-start justify-between w-full p-8 border rounded-lg">
+                <div>
+                    <p> {{ $item['type'] }}</p>
+                    <p class="mt-1 text-3xl font-bold">{{ $item['amount'] }}</p>
+                </div>
+                <x-icon name="{{ $item['icon'] }}" class="w-14 h-14" />
             </div>
-            <x-icon name="{{ $item['icon'] }}" class="w-14 h-14" />
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
