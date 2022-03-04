55b8bcaaf152db56f8db94556db5200ab2ff2583
Koala-Dev-dream <koalah86@gmail.com>
2022-03-04 15:35:35 -0800

    fix: icons for eth2, hydra. link inviete button

--- resources/views/livewire/customer/portfolio/invite.blade.php
+++ resources/views/livewire/customer/portfolio/invite.blade.php
@@ -4,5 +4,5 @@
     <p class="py-3 text-gray-500">
         Get a $10 credit for future ourchases for each friend that buy a tax plane on my crypto tax
     </p>
-    <button class="px-5 py-2 text-sm text-white rounded-lg bg-primary hover:bg-secondary">INVITE FRIENDS</button>
+    <a href="{{ route('customer.invite') }}" class="px-5 py-2 text-sm text-white rounded-lg bg-primary hover:bg-secondary">INVITE FRIENDS</a>
 </div>
