64e803c011d67e59d11bc8bb5f0d48d2f539ee5d
acentior <acentior@outlook.com>
2022-03-09 01:28:33 +0400

    get the bsc testnet balance of the address

--- tests/Feature/CryptoAPITest.php
+++ tests/Feature/CryptoAPITest.php
@@ -30,7 +30,7 @@ class CryptoAPITest extends TestCase
             3,
             0
         );
-        $this->assertTrue(gettype($result) != NULL, 'Can not get any result');
+        $this->assertTrue(gettype($result) === 'object', 'Can not get any result');
         // $this->assertTrue(gettype($result) != NULL, 'Can not get any result');
         var_dump($result['api_version']);
         var_dump($result['request_id']);
@@ -40,5 +40,12 @@ class CryptoAPITest extends TestCase
         var_dump($result['data']['total']);
         var_dump($result['data']['items']);
         // var_dump($result->apiVersion );
+    }
+
+    public function test_bsc_balance() {
+        $this->cryptoApi = new CryptoAPI();
+        $result = $this->cryptoApi->get_balance_bsc('0x0243B2b953F18C7881d0B9A6cA0e4FCf34a22859');
+        $this->assertTrue(gettype($result) === 'object', 'Can not get any result');
+        var_dump($result['data']['item']['confirmed_balance']);
     }
 }
