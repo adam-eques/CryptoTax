66dc3d2ae17d1e8e993bf560a439fe9b4ec75cb6
acentior <acentior@outlook.com>
2022-03-10 23:31:31 +0400

    get transactions and balances in driver

--- app/Cryptos/Drivers/CryptoapisDriver.php
+++ app/Cryptos/Drivers/CryptoapisDriver.php
@@ -3,12 +3,21 @@
 namespace App\Cryptos\Drivers;
 
 use App\Models\CryptoAccount;
+use App\Blockchains\CryptoAPI;
+use Carbon\Carbon;
 
 class CryptoapisDriver implements ApiDriverInterface
 {
+    protected CryptoAccount $account;
+    protected $api;
+
     public static function make(CryptoAccount $account): ApiDriverInterface
     {
-        // TODO: Implement make() method.
+        $obj = new static();
+        $obj->account = $account;
+        $obj->connect();
+
+        return $obj;
     }
 
 
@@ -21,5 +30,71 @@ class CryptoapisDriver implements ApiDriverInterface
     public function updateTransactions(): ApiDriverInterface
     {
         // TODO: Implement updateTransactions() method.
+    }
+
+    protected function connect() : self {
+        $this->api = new CryptoAPI();
+        return $this;
+    }
+
+    public function getApi()
+    {
+        return $this->api;
+    }
+
+    protected function getCredentials() : array
+    {
+        return $this->account->credentials ?: [];
+    }
+
+    public function fetchBalances() {
+        $balances;
+        $credentials = $this->getCredentials();
+        // var_dump($credentials);
+        switch($this->account->cryptoSource->name) {
+            case 'Ethereum Blockchain':
+                $detail = $this->api->get_details($credentials['address'], 'ethereum', 'mainnet', 'balances');
+                $balances = [
+                    'amount' => $detail['data']['item']['confirmed_balance']['amount'],
+                    'ETH' => $detail['data']['item']['confirmed_balance']['unit']
+                ];
+                break;
+            default: break;
+        }
+        return $balances;
+    }
+
+    public function fetchTransactions(Carbon $from = null, Carbon $to = null): array {
+        $transactions = [];
+        $fromTimestamp = 0;
+        $toTimestamp = now()->timestamp;
+        $context = 'not set';
+        $blockchain = '';
+        $network = 'mainnet';
+        $limit = 50;
+        $offset = 0;
+        $total = 0;
+        if ($from) $fromTimestamp = $from->timestamp;
+        if ($to) $toTimestamp = $to->timestamp;
+        $credentials = $this->getCredentials();
+        switch($this->account->cryptoSource->name) {
+            case 'Ethereum Blockchain':
+                $context = 'ethereum';
+                $blockchain = 'ethereum';
+                $network = 'mainnet';
+                break;
+            default: break;
+        }
+
+        do {
+            $response = $this->api->get_transactionsByTime($credentials['address'], $limit, $offset, 'ethereum', $network, $fromTimestamp, $toTimestamp, $context);
+            foreach ($response->data->items as $transaction) {
+                $transactions[] = $transaction;
+            }
+            $total = $response->data->total;
+            $offset += $limit;
+        } while ($offset <= $total);
+
+        return $transactions;
     }
 }
--- tests/Feature/CryptoapisDriverTest.php
+++ tests/Feature/CryptoapisDriverTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Blockchains\CryptoapisDriver;
+use App\Cryptos\Drivers\CryptoapisDriver;
 use App\Models\CryptoAccount;
 
 
@@ -28,5 +28,18 @@ class CryptoapisDriverTest extends TestCase
         $this->assertIsArray($balances, 'Failed to get balance');
         var_dump($balances);
         // var_dump();
+    }
+
+    public function test_getTransactionsTest() {
+        $accounts = CryptoAccount::get();
+        $this->assertTrue($accounts->count()>0, 'Crypto_account table is empty');
+        $account = $accounts[0];
+        $driver = CryptoapisDriver::make($account);
+        $this->assertIsObject($driver, 'Failed to make CryptoapisDriver');
+        $this->assertIsArray($driver->getRequiredCredentials(), 'Failed to get requiredCredentials for api');
+        $this->assertIsObject($driver->getApi(), 'Failed to get api');
+        $transactions = $driver->fetchTransactions();
+        $this->assertIsArray($transactions, 'Failed to get balance');
+        var_dump($transactions);
     }
 }
