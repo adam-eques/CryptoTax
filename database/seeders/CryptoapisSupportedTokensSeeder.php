bbcd6580cab1f4e3cd33f8f819dfd17eb48c707a
acentior <acentior@outlook.com>
2022-03-12 01:09:39 +0400

    cryptoapis_supported_tokens table and seeder added

---
+++ database/seeders/CryptoapisSupportedTokensSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Blockchains\CryptoAPI;
+use App\Models\CryptoapisSuppotedTokens;
+
+class CryptoapisSupportedTokensSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $cryptoApi = new CryptoAPI();
+        $limit = 50;
+        $offset = 0;
+        $total = 0;
+        do {
+            $result = $cryptoApi->get_supported_cryptoAssets($limit, $offset);
+            foreach($result['data']['items'] as $item) {
+                $token = new CryptoapisSuppotedTokens();
+                $token->asset_id = $item["asset_id"];
+                $token->name = $item["asset_name"];
+                $token->symbol = $item["asset_symbol"];
+                $token->type = $item["asset_type"];
+                $token->original_symbol = $item["original_symbol"];
+                $token->raw_data = json_encode($item);
+                $token->save();
+            }
+            $total = $result['data']['total'];
+            $offset += $limit;
+        } while ($offset < $total);
+        var_dump($result);
+    }
+}
