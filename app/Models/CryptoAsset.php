24423b3d383967b8ca19e42c8a91b0dce6d2591b
Bernhard Hörmann <hoermann.b@gmail.com>
2022-03-30 19:05:44 +0200

    FIX: Performance issues

--- app/Models/CryptoAsset.php
+++ app/Models/CryptoAsset.php
@@ -34,7 +34,8 @@ class CryptoAsset extends Model
 
     public function cryptoTransactions(): HasMany
     {
-        return $this->hasMany(CryptoTransaction::class, "currency_id", "crypto_currency_id");
+        return $this->hasMany(CryptoTransaction::class, "currency_id", "crypto_currency_id")
+            ->where("crypto_account_id", $this->crypto_account_id);
     }
 
 
