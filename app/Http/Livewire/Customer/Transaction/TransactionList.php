603809d69d60934f4224ed00c7e9cd16aed43c63
Koala-Dev-dream <koalah86@gmail.com>
2022-03-30 19:39:15 +0300

    fix: query of transaction

--- app/Http/Livewire/Customer/Transaction/TransactionList.php
+++ app/Http/Livewire/Customer/Transaction/TransactionList.php
@@ -129,11 +129,11 @@ class TransactionList extends Component
 
         $transactions = $transactions ->paginate(10);
 
-        $trades_num = auth()->user()->cryptoTransactions()->where('trade_type', 'B')->orWhere('trade_type', 'S')->get()->count();
-        $deposits_num = auth()->user()->cryptoTransactions()->where('trade_type', 'R')->get()->count();
-        $withdrawal_num = auth()->user()->cryptoTransactions()->where('trade_type', 'G')->get()->count();
-        $reviews_num = auth()->user()->cryptoTransactions()->where('trade_type', 'C')->get()->count();
-        $exchange_num = auth()->user()->cryptoTransactions()->where('trade_type', 'E')->get()->count();
+        $trades_num = auth()->user()->cryptoTransactions()->where('trade_type', 'B')->orWhere('trade_type', 'S')->count();
+        $deposits_num = auth()->user()->cryptoTransactions()->where('trade_type', 'R')->count();
+        $withdrawal_num = auth()->user()->cryptoTransactions()->where('trade_type', 'G')->count();
+        $reviews_num = auth()->user()->cryptoTransactions()->where('trade_type', 'C')->count();
+        $exchange_num = auth()->user()->cryptoTransactions()->where('trade_type', 'E')->count();
 
         return view('livewire.customer.transaction.transaction-list', [
             "transactions" => $transactions,
