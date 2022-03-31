9608e4a25397eeff8819e6b839ccc1c824a53ed0
Koala-Dev-dream <koalah86@gmail.com>
2022-03-31 20:55:17 +0300

    UPD: line chart

--- app/Http/Livewire/Customer/Portfolio/LineChart.php
+++ app/Http/Livewire/Customer/Portfolio/LineChart.php
@@ -17,6 +17,7 @@ class LineChart extends Component
     public function mount()
     {
         $this->selected = 5;
+        $this->lineYear = auth()->user()->getPortfolioLineChart('LINE_CHART_YEAR');
     }
 
     public function get_selected($id)
@@ -27,7 +28,7 @@ class LineChart extends Component
     public function render()
     {
         $buttons = [
-            [ 'id' => 0, 'name' => '1D' ],
+            [ 'id' => 0, 'name' => '1D'],
             [ 'id' => 1, 'name' => '1W' ],
             [ 'id' => 2, 'name' => '1M' ],
             [ 'id' => 3, 'name' => '3M' ],
@@ -54,8 +55,8 @@ class LineChart extends Component
                 'value' => [2341, 1242, 1542, 124, 1542, 5432, 1254, 6523, 2341, 1242, 1542, 124, 1542, 5432, 1254, 6523, 1542, 5432, 1254, 6523, 1254, 6523, 1542, 5432, 5432, 1254, 6523, 1254, 6523, 1542],
             ],
             [
-                'label' => ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30'],
-                'value' => [2341, 1242, 1542, 124, 1542, 5432, 1254, 6523, 2341, 1242, 1542, 124, 1542, 5432, 1254, 6523, 1542, 5432, 1254, 6523, 1254, 6523, 1542, 5432, 5432, 1254, 6523, 1254, 6523, 1542],
+                'label' => array_keys($this->lineYear),
+                'value' => array_values($this->lineYear)
 
             ],
             [
